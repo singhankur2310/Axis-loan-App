afc39b4408f5ac874460fb65ac5329ed1aaa37b4
singhankur2310 <singhankur123458@gmail.com>
2025-03-06 22:55:29 +0530

    Bug fix -123 @ 130

--- user.java
+++ user.java
@@ -1,2 +1,24 @@
+<<<<<<< Updated upstream
 // ----------bug-130----------
 	int i = 10;
+=======
+ class user{
+
+	@Getmapping("/welcome")
+	public String WelcomeMsg(){
+		return "Hello Ankur";
+}
+
+
+	//-------/Bug - 123------
+
+
+	@Getmapping("/greet")
+	public String greetMsg(){
+		//In progress .............		
+
+}
+
+
+}
+>>>>>>> Stashed changes
